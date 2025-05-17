11557d30fb1fb418909c924be6cf82e087728577
gresadaqi <gresa.daqi11@gmail.com>
2025-05-17 16:06:29 +0200

    Update Loggerutil.java

--- src/hmac/LoggerUtil.java
+++ src/hmac/LoggerUtil.java
@@ -1,17 +1,38 @@
 package hmac;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.*;
 
 public class LoggerUtil {
-    private static Logger logger = Logger.getLogger("HMACLogger");
+    private static final Logger logger = Logger.getLogger("HMACLogger");
 
     static {
         try {
-            FileHandler fh = new FileHandler("log.txt", true);
-            fh.setFormatter(new SimpleFormatter());
-            logger.addHandler(fh);
+
+            String date = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+            String fileName = "log-" + date + ".txt";
+
+
+            FileHandler fileHandler = new FileHandler(fileName, true);
+
+
+            fileHandler.setFormatter(new Formatter() {
+                @Override
+                public String format(LogRecord record) {
+                    return String.format("[%s] [%s] %s: %s%n",
+                            new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(record.getMillis())),
+                            record.getLevel().getName(),
+                            record.getSourceClassName(),
+                            record.getMessage());
+                }
+            });
+
+
+            logger.addHandler(fileHandler);
             logger.setUseParentHandlers(false);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
